11bcd0682b2e85e8468bb050631d7c19053c91cb
Lucasm4sco <lmbp202@gmail.com>
2022-12-11 13:36:19 -0300

    creating routes and html of the registration page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
 const App = () => {
-  return <div className='App'>Hello!</div>
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes> 
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
